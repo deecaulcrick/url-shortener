d0ec23403565fb1200d14d64d5f135201ec291e5
Dee Caulcrick <deecaulcrick@gmail.com>
2024-04-14 09:02:52 +0100

    major edits

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,13 +19,48 @@ app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+const urlDatabase = {};
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
 });
 
 // Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+
+app.post("/api/shorturl", async (req, res) => {
+  const url = req.body.url.trim();
+
+  function isValidUrl(url) {
+    // Regular expression for URL validation
+    const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
+    return urlPattern.test(url);
+  }
+
+  if (!isValidUrl(url)) {
+    res.json({
+      error: "invalid url",
+    });
+  } else {
+    ShortUrl.create({ full: req.body.url });
+    const shortUrl = await ShortUrl.findOne({ full: req.body.url });
+    res.json({
+      original_url: req.body.url,
+      short_url: shortUrl.short,
+    });
+  }
+});
+
+app.get("/api/:shorturl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shorturl });
+  console.log(shortUrl);
+
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
 });
 
 app.listen(port, function () {
